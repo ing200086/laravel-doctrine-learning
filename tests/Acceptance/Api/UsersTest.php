f5fcd36bddf2e465a265b68af3bb310593e1bbd7
= <=>
2016-12-19 09:50:42 -0600

    Add test to verify Json structure on users

--- tests/Acceptance/Api/UsersTest.php
+++ tests/Acceptance/Api/UsersTest.php
@@ -7,10 +7,25 @@ class UsersTest extends TestCase
     /**
      * @test
      */
-    public function it_loads_all_users()
+    public function it_gives_a_good_response_to_all_users_uri()
     {
         $this->getJson('api/v1/user');
-
         $this->assertResponseOk();
+    }
+
+    /**
+     * @test
+     */
+    public function it_gets_all_reported_bugs_with_users()
+    {
+    	$this->getJson('api/v1/user');
+
+    	$users = $this->response->getData('data')['data'];
+
+    	$this->seeJsonStructure([
+					'*' => [
+						'name'
+					]
+    			], $users);
     }
 }
